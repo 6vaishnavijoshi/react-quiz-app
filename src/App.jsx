62a6a8736d70729e3befd18b918f32f0ec4cd802
Vaishnavi Veerupakshabhatt Joshi <vaishnavivjoshi290@gmail.com>
2026-06-20 13:31:37 +0530

    Added questions array

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,46 @@
 function App() {
+  const questions = [
+    {
+      question: "What is React?",
+      options: [
+        "Java",
+        "Python",
+        "JavaScript Library",
+        "C++"
+      ],
+      answer: "JavaScript Library"
+    },
+
+    {
+      question: "Who developed React?",
+      options: [
+        "Google",
+        "Facebook",
+        "Microsoft",
+        "Amazon"
+      ],
+      answer: "Facebook"
+    }
+  ];
+
   return (
     <div>
       <h1>React Quiz App</h1>
 
-      <h2>What is React?</h2>
+      <h2>
+        {questions[0].question}
+      </h2>
 
-      <button>Java</button>
-      <button>Python</button>
-      <button>JavaScript Library</button>
-      <button>C++</button>
+      {questions[0].options.map(
+        (option) => (
+          <button key={option}>
+            {option}
+          </button>
+        )
+      )}
 
-      <br /><br />
+      <br />
+      <br />
 
       <button>Next</button>
     </div>
