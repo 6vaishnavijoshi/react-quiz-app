fb1c1fd9b793ca19e9b6e1691096813804d45e8d
Vaishnavi Veerupakshabhatt Joshi <vaishnavivjoshi290@gmail.com>
2026-06-20 13:24:06 +0530

    Quiz App UI completed

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <div>
+      <h1>React Quiz App</h1>
+
+      <h2>What is React?</h2>
+
+      <button>Java</button>
+      <button>Python</button>
+      <button>JavaScript Library</button>
+      <button>C++</button>
+
+      <br /><br />
+
+      <button>Next</button>
+    </div>
+  );
+}
+
+export default App;
